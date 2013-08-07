7ae8afee9cec9007300db477e33a7c5f979e7a57
170warship <j10a.student@sina.com>
2013-08-07 10:21:21 +0800

    Initial Commit

---
+++ CocosShow/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CocosShowAppController.h
+//  CocosShow
+//
+//  Created by 170warship on 8/7/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
